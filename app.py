d40f3b89c95e20895477c36a8798f4685a3e713d
Andy Cheung <77382250+andy3278@users.noreply.github.com>
2023-10-28 19:22:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 import panel as pn
 from panel.chat import ChatInterface
 
-pn.extension("perspective")
+pn.extension()
+
+def echo(contents, user, instance):
+    return contents
+
+chat_interface = ChatInterface(
+    callback=echo, 
+    user="EchoBot",
+    avatar="?",
+    callback_user="Counter",)
+
+chat_interface.servable()
